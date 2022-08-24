3fd0f4c1a79720c301290dddd97fc639e990df71
Ezio Santori <Ezio.Santori@wolterskluwer.com>
2022-08-24 16:32:19 +0200

    added matrial ui

--- apps/microapp-01/src/app/app.tsx
+++ apps/microapp-01/src/app/app.tsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
+import Button from '@mui/material/Button';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="microapp-01" />
+      {/* <NxWelcome title="microapp-01" /> */}
+      <Button variant="contained">Hello World</Button>
       <div />
     </>
   );
